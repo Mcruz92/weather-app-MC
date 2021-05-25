b3948a048784055f7b95cc6cf36c083df583d2c2
Mcruz92 <81129725+Mcruz92@users.noreply.github.com>
2021-05-24 23:01:24 -0400

    display forecast

--- src/app.js
+++ src/app.js
@@ -30,7 +30,8 @@ function displayForecast(response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  forecast.forEach(function (forecastDay) {
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {}
     forecastHTML =
       forecastHTML +
       `
